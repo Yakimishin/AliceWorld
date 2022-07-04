9a9bb260845c47be8f9b4a893c2ba2f9aff13795
Yakimishin <yakimishinruslan@yandex.ru>
2022-07-04 09:12:03 +0300

    gift

--- js/main.js
+++ js/main.js
@@ -1,17 +1,20 @@
 var popup = document.querySelector('.popup');
 var dairy = document.querySelector('.dairy');
 var startButton = document.querySelector('.start-button');
+var presentWords = document.querySelector('.present')
 
 function hide(){
 	startButton.classList.add('hidden');
 	popup.classList.add('hidden');
+	presentWords.classList.add('hidden')
 }
 
 
 startButton.addEventListener('click', function(){
 	startButton.classList.add('close-button');
+	presentWords.classList.add('close-present');
 	popup.classList.add('close-popup');
 	dairy.classList.remove('hidden');
 	dairy.classList.add('show-dairy');
-	setTimeout(hide, 5*1000);
+	setTimeout(hide, 7*1000);
 })
